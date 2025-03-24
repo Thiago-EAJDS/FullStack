01478f9d74e79e3406ba664d42d7b28d52cfc696
Thiago Emanuel <thiagoeajds@gmail.com>
2025-03-24 17:41:37 -0300

    v2

--- cartões pessoais/Cartao em 3D/script.js
+++ cartões pessoais/Cartao em 3D/script.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const darkModeToggle = document.getElementById("darkModeToggle");
     const cartao = document.getElementById("cartao");
     const body = document.body;
-    const info = document.querySelector(".info");
     const icon = document.getElementById("icon");
-    let isFlipped = true;
+
+    let rotateX = 0;
+    let rotateY = 0;
+    let rotateZ = 0;
 
     darkModeToggle.addEventListener("click", () => {
         body.classList.toggle("dark-mode");
         cartao.classList.toggle("dark-mode");
-        info.classList.toggle("dark-mode-text");
         darkModeToggle.classList.toggle("dark-mode-button");
-        
+
         if (body.classList.contains("dark-mode")) {
             icon.textContent = "☀️";
         } else {
@@ -20,21 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     cartao.addEventListener("mousemove", (e) => {
-        let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-        let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-        cartao.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        let centerX = window.innerWidth / 2;
+        let centerY = window.innerHeight / 2;
+
+        rotateY = (e.pageX - centerX) / 1;
+        
+        rotateX = (e.pageY - centerY) / 1;
+
+        cartao.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
     });
 
     cartao.addEventListener("mouseleave", () => {
-        cartao.style.transform = "rotateY(0deg) rotateX(0deg)";
+        cartao.style.transform = `rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
     });
 
-    cartao.addEventListener("click", () => {
-        if (!isFlipped) {
-            cartao.style.transform += " rotateY(180deg)";
+    darkModeToggle.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+        cartao.classList.toggle("dark-mode");
+        darkModeToggle.classList.toggle("dark-mode-button");
+
+        if (body.classList.contains("dark-mode")) {
+            icon.textContent = "☀️";
         } else {
-            cartao.style.transform = "rotateY(0deg) rotateX(0deg)";
+            icon.textContent = "🌙";
         }
-        isFlipped = !isFlipped;
     });
 });
